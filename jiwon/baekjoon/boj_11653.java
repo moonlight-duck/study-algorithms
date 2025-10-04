e027d8a3b77385f404b31da4f3dffe6b59e33652
unauthorized-401 <moonlight_youlike@naver.com>
2025-10-04 15:01:29 +0900

    feat: 코드 개선

--- jiwon/baekjoon/boj_11653.java
+++ jiwon/baekjoon/boj_11653.java
@@ -3,48 +3,28 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class boj_11653 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
 
-        getPrimeFactorization(num);
+        if (num > 1) {
+            getPrimeFactorization(num);
+        }
     }
 
     private static void getPrimeFactorization(int num) {
-        if (num > 1) {
-            List<Integer> primes = new ArrayList<>();
-            primes.add(2);
-
-            // 소수 구하기
-            for (int i = 3; i <= num; i+=2) {
-                boolean isPrime = true;
-                for (Integer prime : primes) {
-                    if (i % prime == 0) {
-                        isPrime = false;
-                        break;
-                    }
-                }
-
-                if (isPrime) {
-                    primes.add(i);
-                }
+        for (int i = 2; i * i <= num; i++) {
+            while (num % i == 0) {
+                System.out.println(i);
+                num /= i;
             }
+        }
 
-            // 소인수분해 하기
-            for (Integer prime : primes) {
-                while (num != 0) {
-                    if (num % prime == 0) {
-                        num /= prime;
-                        System.out.println(prime);
-                    } else {
-                        break;
-                    }
-                }
-            }
+        // 마지막으로 남은 수 고려
+        if  (num > 1) {
+            System.out.println(num);
         }
     }
 }
