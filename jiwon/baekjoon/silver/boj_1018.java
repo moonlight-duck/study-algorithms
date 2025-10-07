d11aea68d5b79bcd69994fce6c4af833895bb232
unauthorized-401 <moonlight_youlike@naver.com>
2025-10-08 03:24:58 +0900

    feat: 1018 해결

--- jiwon/baekjoon/silver/boj_1018.java
+++ jiwon/baekjoon/silver/boj_1018.java
@@ -1,4 +1,4 @@
-package baekjoon;
+package baekjoon.silver;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,36 +12,46 @@ public class boj_1018 {
         int n = Integer.parseInt(token.nextToken());
         int m = Integer.parseInt(token.nextToken());
 
-        String[][] arrays = new String[n][m];
+        int count = 2500;
 
+        // 입력값 배열에 넣기
+        String[][] arrays = new String[n][m];
         for (int i = 0; i < n; i++) {
             String line = br.readLine();
             arrays[i] = line.split("");
         }
 
-        int case1ChangeCount = getChangeCount("W", n, m, arrays);
-        int case2ChangeCount = getChangeCount("B", n, m, arrays);
+        // 체스판을 조정하면서 개수 구하기
+        for (int i = 0; i <= (m-8); i++) {
+            for (int j = 0; j <= (n-8); j++) {
+                int case1ChangeCount = getChangeCount("W", i, j, arrays);
+                int case2ChangeCount = getChangeCount("B", i, j, arrays);
 
-        System.out.println(Math.min(case1ChangeCount, case2ChangeCount));
+                int min = Math.min(case1ChangeCount, case2ChangeCount);
+                count = Math.min(count, min);
+            }
+        }
 
+        System.out.println(count);
     }
 
+    // 주어진 좌표에서 수정해야 할 칸 수 구하기
     private static int getChangeCount(String color, int n, int m, String[][] arrays) {
         int changeCount = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        for (int i = m; i < (m+8); i++) {
+            for (int j = n; j < (n+8); j++) {
                 if (!Objects.equals(arrays[i][j], color)) {
                     changeCount += 1;
                 }
                 color = changeColor(color);
             }
-            if (m % 2 == 0) {
-                color = changeColor(color);
-            }
+            // 다음 줄로 넘어갈 때
+            color = changeColor(color);
         }
         return changeCount;
     }
 
+    // 색 바꾸기
     private static String changeColor(String color) {
         if (Objects.equals(color, "W")) return "B";
         else return "W";
