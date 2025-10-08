c2976695292c6850505e2dbbea9e53132ebba17d
unauthorized-401 <moonlight_youlike@naver.com>
2025-10-08 15:04:48 +0900

    feat: 10989 마무리

--- jiwon/baekjoon/bronze/boj_10989.java
+++ jiwon/baekjoon/bronze/boj_10989.java
@@ -1,27 +1,25 @@
-package baekjoon;
+package baekjoon.bronze;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
-public class boj_2751 {
+public class boj_10989 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        List<Integer> list = new ArrayList<>();
+        int[] array = new int[n];
         for (int i = 0; i < n; i++) {
-            list.add(Integer.parseInt(br.readLine()));
+            array[i] = Integer.parseInt(br.readLine());
         }
 
-        Collections.sort(list);
+        Arrays.sort(array);
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            sb.append(list.get(i)).append("\n");
+            sb.append(array[i]).append("\n");
         }
         System.out.println(sb);
     }
