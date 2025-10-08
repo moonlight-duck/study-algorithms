cdf7f07dfae77f7cf4e12ec20add6093aa465f00
unauthorized-401 <moonlight_youlike@naver.com>
2025-10-08 12:58:58 +0900

    feat: 2751 1차 커밋

---
+++ jiwon/baekjoon/boj_2751.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class boj_2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(list.get(i));
+        }
+    }
+}
