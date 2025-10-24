ebc68b0f5f026fe93e2fe02b36b5c8c47168cb80
moonlight-duck <moonlight_youlike@naver.com>
2025-10-24 17:16:22 +0900

    feat: Deque 써보기

---
+++ jiwon/baekjoon/silver/boj_18258.java
@@ -0,0 +1,62 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class boj_18258 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Deque<Integer> queue = new LinkedList<>();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer token = new StringTokenizer(br.readLine(), " ");
+            String command = token.nextToken();
+
+            switch (command) {
+                case "push":
+                    int number = Integer.parseInt(token.nextToken());
+                    queue.add(number);
+                    break;
+                case "pop":
+                    if (queue.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(queue.pop()).append("\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(queue.size()).append("\n");
+                    break;
+                case "empty":
+                    if (queue.isEmpty()) {
+                        sb.append(1).append("\n");
+                    } else {
+                        sb.append(0).append("\n");
+                    }
+                    break;
+                case "front":
+                    if (queue.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(queue.peekFirst()).append("\n");
+                    }
+                    break;
+                case "back":
+                    if (queue.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(queue.peekLast()).append("\n");
+                    }
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+}
