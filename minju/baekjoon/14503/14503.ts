0fc01f9937efb19a319a161e29491643b2be1f8a
탁민주 <takminjoo@gmail.com>
2025-12-01 08:17:43 +0900

    feat: 백준 14503 코드 개선

--- minju/baekjoon/14503/14503.ts
+++ minju/baekjoon/14503/14503.ts
@@ -9,8 +9,8 @@ const room: number[][] = input
   .slice(2, 2 + n)
   .map((x) => x.split(" ").map(Number));
 
-const dx: number[] = [0, 1, 0, -1]; // 북, 동 남, 서
-const dy: number[] = [-1, 0, 1, 0];
+const dx: number[] = [-1, 0, 1, 0]; // 북, 동 남, 서
+const dy: number[] = [0, 1, 0, -1];
 
 let cnt: number = 0;
 
@@ -19,24 +19,19 @@ while (c < m && c >= 0 && r < n && r >= 0) {
 
   // 1. 현재 칸이 아직 청소되지 않은 경우, 현재 칸을 청소한다.
   if (room[r][c] === 0) {
-    console.log("청소했음", r, c);
     room[r][c] = 2;
     ++cnt;
   }
 
   let isAllClean = true;
-  console.log("초기화");
   for (let i = 3; i >= 0; i--) {
     const nd = (d + i) % DIREACT;
     const nx = r + dx[nd];
     const ny = c + dy[nd];
 
-    if (nx >= m || nx < 0 || ny >= n || ny < 0) continue;
-
-    console.log("방향 확인중", nd);
+    if (nx >= n || nx < 0 || ny >= m || ny < 0) continue;
 
     if (room[nx][ny] === 0) {
-      console.log("청소 안되어 있는 방 발견", nx, ny);
       r = nx;
       c = ny;
       d = nd;
@@ -48,8 +43,6 @@ while (c < m && c >= 0 && r < n && r >= 0) {
   if (isAllClean) {
     r = r - dx[d];
     c = c - dy[d];
-
-    console.log("4칸 모두 청소 되어 있음, 후진함", c, r);
   }
 
   if (room[r][c] === 1) {
