66f5b1f2f8b82a85e5cc6211bbbed60fc45b1883
탁민주 <takminjoo@gmail.com>
2025-12-16 16:32:42 +0900

    docs: 오타 수정

--- minju/baekjoon/1546/1546.ts
+++ minju/baekjoon/1546/1546.ts
@@ -1,5 +1,5 @@
 /**
- * 제 정답과 출력값의 절대오차 또는 상대오차가 10-2 이하이면 정답이다.
+ * 실제 정답과 출력값의 절대오차 또는 상대오차가 10-2 이하이면 정답이다.
  **/
 import { readFileSync } from "fs";
 const input = readFileSync("/dev/stdin").toString().trim().split("\n");
