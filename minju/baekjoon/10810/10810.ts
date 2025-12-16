1fa91e5198a8b58295a555c75a27d832f3846a7a
탁민주 <takminjoo@gmail.com>
2025-12-16 15:00:49 +0900

    feat: 백준 10810번 문제 풀이

--- minju/baekjoon/10810/10810.ts
+++ minju/baekjoon/10810/10810.ts
@@ -1,2 +1,15 @@
 import { readFileSync } from "fs";
 const input = readFileSync("/dev/stdin").toString().trim().split("\n");
+const [n, m] = input[0].split(" ").map(Number);
+const data = input.slice(1);
+
+const result = Array.from({ length: n + 1 }, () => 0);
+
+for (let index = 0; index < m; index++) {
+  const [i, j, k] = data[index].split(" ").map(Number);
+  for (let range = i; range <= j; range++) {
+    result[range] = k;
+  }
+}
+
+console.log(result.slice(1).join(" "));
