a1fd484fdb35901900401bcc905c31eeb87f6cd6
탁민주 <takminjoo@gmail.com>
2026-01-29 10:33:58 +0900

    feat: 백준 단계별 문제풀이(기하 1: 직사각형과 삼각형) - 3009 코드 개선

--- minju/baekjoon/3009/3009.ts
+++ minju/baekjoon/3009/3009.ts
@@ -1,25 +1,14 @@
 import { readFileSync } from "fs";
 const input = readFileSync("/dev/stdin").toString().trim().split("\n");
-const checkX = new Map();
-const checkY = new Map();
+
+let rX = 0;
+let rY = 0;
 
 for (let i = 0; i < input.length; i++) {
-  const [x, y] = input[i].split(" ").map(Number);
+  const [x, y] = input[i].trim().split(" ").map(Number);
 
-  if (checkX.has(x)) {
-    checkX.set(x, checkX.get(x) + 1);
-  } else {
-    checkX.set(x, 1);
-  }
-
-  if (checkY.has(y)) {
-    checkY.set(y, checkY.get(y) + 1);
-  } else {
-    checkY.set(y, 1);
-  }
+  rX ^= x;
+  rY ^= y;
 }
-
-const rX = [...checkX].find(([, val]) => val === 1)?.[0];
-const rY = [...checkY].find(([, val]) => val === 1)?.[0];
 
 console.log(`${rX} ${rY}`);
