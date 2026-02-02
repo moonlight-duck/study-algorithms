8193f18b0d7fe3683883239f55c1c1bab81cf943
탁민주 <takminjoo@gmail.com>
2026-02-02 22:26:33 +0900

    feat: 백준 단계별 문제풀이(집합) - 11478

---
+++ minju/baekjoon/11478/11478.test.ts
@@ -0,0 +1,14 @@
+import { describe, it, expect } from "vitest";
+
+import { runBaekjoonTest } from "../../testHelper.js";
+
+const FILE_NAME = "11478";
+
+describe("백준 11478번 - 공식 테스트 케이스", async () => {
+  it("예제 1", async () => {
+    const input = `ababc`;
+
+    const result = await runBaekjoonTest(input, FILE_NAME);
+    expect(result).toBe(`12`);
+  });
+});
