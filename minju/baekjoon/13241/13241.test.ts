64417fe4b3e0dbc06542ed66b071a0073b4222eb
탁민주 <takminjoo@gmail.com>
2026-02-03 00:09:31 +0900

    feat: 백준 단계별 문제풀이(약수, 배수와 소수 2) - 13241

---
+++ minju/baekjoon/13241/13241.test.ts
@@ -0,0 +1,21 @@
+import { describe, it, expect } from "vitest";
+
+import { runBaekjoonTest } from "../../testHelper.js";
+
+const FILE_NAME = "13241";
+
+describe("백준 13241번 - 공식 테스트 케이스", async () => {
+  it("예제 1", async () => {
+    const input = `1 1`;
+
+    const result = await runBaekjoonTest(input, FILE_NAME);
+    expect(result).toBe(`1`);
+  });
+
+  it("예제 2", async () => {
+    const input = `3 5`;
+
+    const result = await runBaekjoonTest(input, FILE_NAME);
+    expect(result).toBe(`15`);
+  });
+});
