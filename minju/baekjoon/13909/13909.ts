b52347340bd56c41a26a056d990607a685ab8681
탁민주 <takminjoo@gmail.com>
2026-02-03 16:09:12 +0900

    feat: 백준 단계별 문제풀이(약수, 배수와 소수 2) - 13909 메모리 초과개선(완전제곱수 구하기)

--- minju/baekjoon/13909/13909.ts
+++ minju/baekjoon/13909/13909.ts
@@ -1,13 +1,9 @@
 import { readFileSync } from "fs";
 const input = Number(readFileSync("/dev/stdin").toString().trim());
 
-const window = new Array(input + 1).fill(true);
-window[0] = false;
-
-for (let i = 2; i <= input; i++) {
-  for (let j = i; j <= input; j += i) {
-    window[j] = !window[j];
-  }
+let cnt = 0;
+for (let i = 1; i <= Math.sqrt(input); i++) {
+  cnt++;
 }
 
-console.log(window.filter((item) => item).length);
+console.log(cnt);
