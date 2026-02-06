6fd416e4d45a90611c3da8a0d30e342d499995a3
탁민주 <takminjoo@gmail.com>
2026-02-06 13:16:21 +0900

    feat: 백준 단계별 문제풀이(조합론) - 1010 재시도(n,m 바뀜, 타입 범위 벗어남)

--- minju/baekjoon/1010/1010.ts
+++ minju/baekjoon/1010/1010.ts
@@ -4,13 +4,13 @@ const input = readFileSync("/dev/stdin").toString().trim().split("\n");
 const result = [];
 
 for (let j = 1; j < input.length; j++) {
-  const [m, n] = input[j].split(" ").map(Number);
+  const [n, m] = input[j].split(" ").map(Number);
 
-  let top = 1;
-  let bottom = 1;
+  let top = BigInt(1);
+  let bottom = BigInt(1);
   for (let i = 0; i < n; i++) {
-    top *= m - i;
-    bottom *= i + 1;
+    top *= BigInt(m - i);
+    bottom *= BigInt(i + 1);
   }
   result.push(top / bottom);
 }
