ae291240f271403bb6c5e676fc5c9bf76ad16688
탁민주 <takminjoo@gmail.com>
2026-02-20 17:16:37 +0900

    feat: 백준 단계별 문제풀이(누적 합) - 11659 시간초과 재시도

--- minju/baekjoon/11659/11659.ts
+++ minju/baekjoon/11659/11659.ts
@@ -1,15 +1,16 @@
 import { readFileSync } from "fs";
 const input = readFileSync("/dev/stdin").toString().trim().split("\n");
 const data = input[1].split(" ").map(Number);
+const dp = Array.from({ length: data.length + 1 }, () => 0);
+
+for (let i = 0; i < data.length; i++) {
+  dp[i + 1] = dp[i] + data[i];
+}
 
 const result = [];
 for (let i = 2; i < input.length; i++) {
   const [start, end] = input[i].split(" ").map(Number);
-  let sum = 0;
-  for (let j = start - 1; j <= end - 1; j++) {
-    sum += data[j];
-  }
-  result.push(sum);
+  result.push(dp[end] - dp[start - 1]);
 }
 
 console.log(result.join("\n"));
