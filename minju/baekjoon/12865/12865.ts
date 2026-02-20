98f7ad73bdd002efc615a487aa67410d8256378e
탁민주 <takminjoo@gmail.com>
2026-02-20 16:52:02 +0900

    feat: 백준 단계별 문제풀이(동적 계획법 1) - 12865 재풀이

--- minju/baekjoon/12865/12865.ts
+++ minju/baekjoon/12865/12865.ts
@@ -1,29 +1,14 @@
 import { readFileSync } from "fs";
 const input = readFileSync("/dev/stdin").toString().trim().split("\n");
 const [n, k] = input[0].split(" ").map(Number);
-const data: number[][] = [];
 
-for (let i = 1; i < input.length; i++) {
-  const [w, v] = input[i].split(" ").map(Number);
-  data.push([w, v]);
+const dp = new Array(k + 1).fill(0);
+
+for (let i = 1; i <= n; i++) {
+  const [weight, value] = input[i].split(" ").map(Number);
+  for (let w = k; w >= weight; w--) {
+    dp[w] = Math.max(dp[w], dp[w - weight] + value);
+  }
 }
 
-let i = 0;
-let max = 0;
-while (i < n) {
-  let w = 0;
-  let v = 0;
-  for (let j = i; j < n; j++) {
-    if (data[j][0] + w <= k) {
-      w += data[j][0];
-      v += data[j][1];
-    }
-  }
-
-  if (max < v) {
-    max = v;
-  }
-  i++;
-}
-
-console.log(max);
+console.log(dp[k]);
