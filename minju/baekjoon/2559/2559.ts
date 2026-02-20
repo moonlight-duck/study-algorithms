cc4ec4e73fb19d33aaee6253dd4ceb3d12891f80
탁민주 <takminjoo@gmail.com>
2026-02-20 17:50:50 +0900

    feat: 백준 단계별 문제풀이(누적 합) - 2559 시간초과 개선 재시도

--- minju/baekjoon/2559/2559.ts
+++ minju/baekjoon/2559/2559.ts
@@ -4,15 +4,16 @@ const data = input[1].split(" ").map(Number);
 
 const [n, k] = input[0].split(" ").map(Number);
 const dp = Array.from({ length: n - k + 1 }, () => 0);
-let max = 0;
 
-for (let i = 0; i < dp.length; i++) {
-  for (let j = i; j < i + k; j++) {
-    dp[i] += data[j];
-  }
-  if (max < dp[i]) {
-    max = dp[i];
-  }
+for (let i = 0; i < k; i++) {
+  dp[0] += data[i];
+}
+
+let max = dp[0];
+
+for (let i = 1, j = k; i <= dp.length && j < data.length; i++, j++) {
+  dp[i] = dp[i - 1] + data[j] - data[j - k];
+  if (max < dp[i]) max = dp[i];
 }
 
 console.log(max);
