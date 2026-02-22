2152eda6bd60d30766506c624b5dd2e651dc357b
moonlight-duck <moonlight_youlike@naver.com>
2026-02-22 16:24:34 +0900

    feat: 하노이 마무리

---
+++ jiwon/baekjoon/boj_11729.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_11729 {
+    static StringBuilder answer = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        hanoi(n, 1, 2, 3);
+
+        System.out.println((int)Math.pow(2, n) - 1);
+        System.out.print(answer);
+    }
+
+    public static void hanoi(int n, int start, int mid, int end) {
+        if (n == 1) {
+            answer.append(start).append(" ").append(end).append("\n");
+            return;
+        }
+
+        hanoi(n - 1, start, end, mid);
+        answer.append(start).append(" ").append(end).append("\n");
+        hanoi(n - 1, mid, start, end);
+    }
+}
