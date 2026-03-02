219031928dc7dac2b84042df26d3a419c9fee83d
moonlight-duck <moonlight_youlike@naver.com>
2026-03-02 15:28:31 +0900

    feat: 중복 허용이면 방문 여부 필요 없음

---
+++ jiwon/baekjoon/silver/boj_15651.java
@@ -0,0 +1,38 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_15651 {
+    public static StringBuilder sb = new StringBuilder();
+    public static int[] result;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        result = new int[m];
+
+        dfs(n, m, 0);
+
+        System.out.print(sb);
+    }
+
+    private static void dfs(int n, int m, int depth) {
+        if (m == depth) {
+            for (int num : result) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            result[depth] = i + 1;
+            dfs(n, m, depth + 1);
+        }
+    }
+}
