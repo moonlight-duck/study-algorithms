99153f169d8eac9c77f89a2f8de03b6684f065cd
탁민주 <takminjoo@gmail.com>
2026-03-06 18:07:46 +0900

    feat: 백준 단계별 문제풀이(그리디 알고리즘 1) - 13305 부분점수 개선

--- minju/baekjoon/13305/13305.ts
+++ minju/baekjoon/13305/13305.ts
@@ -1,18 +1,18 @@
 import { readFileSync } from "fs";
 const input = readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const distance = input[1].split(" ").map(Number);
-const cityPrice = input[2].split(" ").map(Number);
+const distance = input[1].split(" ").map(BigInt);
+const cityPrice = input[2].split(" ").map(BigInt);
 
 let prevPrice = cityPrice[0];
-let totalPrice = cityPrice[0] * distance[0];
-for (let i = 1; i < distance.length; i++) {
-  if (prevPrice * distance[i] < cityPrice[i] * distance[i]) {
-    totalPrice += prevPrice * distance[i];
-  } else {
-    totalPrice += cityPrice[i] * distance[i];
+let totalPrice = 0n;
+
+for (let i = 0; i < distance.length; i++) {
+  if (cityPrice[i] < prevPrice) {
     prevPrice = cityPrice[i];
   }
+
+  totalPrice += prevPrice * distance[i];
 }
 
-console.log(totalPrice);
+console.log(totalPrice.toString());
