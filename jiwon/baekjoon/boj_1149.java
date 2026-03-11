13c4a3f9d28d9b96c21e076b8f540604543b223d
moonlight-duck <moonlight_youlike@naver.com>
2026-03-11 20:35:22 +0900

    feat: 점화식 연습

---
+++ jiwon/baekjoon/boj_1149.java
@@ -0,0 +1,36 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_1149 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] cost = new int[n][3];
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            cost[i][0] = Integer.parseInt(st.nextToken());  // R
+            cost[i][1] = Integer.parseInt(st.nextToken());  // G
+            cost[i][2] = Integer.parseInt(st.nextToken());  // B
+        }
+
+        int[][] dp = new int[n][3];
+        dp[0][0] = cost[0][0];
+        dp[0][1] = cost[0][1];
+        dp[0][2] = cost[0][2];
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + cost[i][0];   // R
+            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + cost[i][1];   // G
+            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + cost[i][2];   // B
+        }
+
+        int result = Math.min(dp[n-1][0], Math.min(dp[n-1][1], dp[n-1][2]));
+        System.out.println(result);
+    }
+}
